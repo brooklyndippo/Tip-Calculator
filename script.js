f95c08c34c1d0c6f8bd734548d5bd535a0e716bd
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-09-21 22:15:19 -1000

    wrote function to calculate tip with latest input values -- debugged & proved working in console.log

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+//-----------------------------------------------
+// SELECT INPUTS/OUTPUTS FROM HTML DOC
 const billInput = document.querySelector('#input-bill')
 const tipInput = document.querySelector('#input-tip')
 const peopleInput = document.querySelector('#input-people')
 const displayTip = document.querySelector('#display-tip')
 const displyTotal = document.querySelector('#display-total')
 
-billInput.addEventListener('input', handleBill) 
-tipInput.addEventListener('input', handleBill)
-peopleInput.addEventListener('input', handleBill)
+//-----------------------------------------------
+// ADD EVENT LISTENERS
+billInput.addEventListener('change', handleBill) 
+tipInput.addEventListener('change', handleBill)
+peopleInput.addEventListener('change', handleBill)
 
-const bill = parseFloat(billInput.value)
-const tip = parseInt(tipInput.value)
-const numPeople = parseInt(peopleInput.value)
+//-----------------------------------------------
+// FUNCTION TO CALCULATE TIP/TOTAL WITH MOST RECENT VALUES
+function handleBill(e) {
 
+    //collect the most updated input for all values
+    const bill = parseFloat(billInput.value)
+    const tip = parseInt(tipInput.value)
+    const numPeople = parseInt(peopleInput.value)
+
+    //using inputs calculate different outputs & log in console to test
+    const tipOutput = (bill * tip / 100)
+    const totalOutput = (bill + tipOutput)
+    const cppOutput = totalOutput / numPeople
+    console.log (tipOutput)
+    console.log (totalOutput)
+    console.log (cppOutput)
+}
