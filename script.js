059806f5ba4c428905b775d072b1a5b0971438b1
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-09-21 21:51:51 -1000

    define variables for the inputs and outputs in script

---
+++ script.js
@@ -0,0 +1,6 @@
+const billInput = document.querySelector('#input-bill')
+const tipInput = document.querySelector('#input-tip')
+const peopleInput = document.querySelector('#input-people')
+const displayTip = document.querySelector('#display-tip')
+const displyTotal = document.querySelector('#display-total')
+
