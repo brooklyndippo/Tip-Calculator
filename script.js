c612f533e2cca24dc2c55aae1cf724d72d6080fc
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-09-21 21:59:10 -1000

    add event listeners and convert input to int/float

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const peopleInput = document.querySelector('#input-people')
 const displayTip = document.querySelector('#display-tip')
 const displyTotal = document.querySelector('#display-total')
 
+billInput.addEventListener('input', handleBill) 
+tipInput.addEventListener('input', handleBill)
+peopleInput.addEventListener('input', handleBill)
+
+const bill = parseFloat(billInput.value)
+const tip = parseInt(tipInput.value)
+const numPeople = parseInt(peopleInput.value)
+
